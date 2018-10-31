662260b41323786018caf1f4442866f6fdac1fd1
amir.hussain <amir.hussain@mobigesture.com>
2018-10-31 14:50:41 -0700

    validation for login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from 'src/app/validations/password-validation';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
 
-  ngOnInit() {
+  get email() {
+    return this.loginForm.get('email');
   }
 
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  constructor(private fb : FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      'email': ['',[Validators.required, forbiddenNameValidator(/^[a-zA-Z0-9.!#$%&amp;'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)]],
+      'password': ['', Validators.required]
+    })
+  }
+
+  login(formData) {
+    console.log(formData);
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+  }
 }
